2d5591cdca6c9289d7e0d55af44e746a10f8911f
Louis Maddox <louismmx@gmail.com>
2025-04-11 18:55:37 +0100

    feat: working demo

--- build.rs
+++ build.rs
@@ -7,7 +7,7 @@ fn main() {
         println!("cargo:warning=Building for wasm32-unknown-emscripten target");
         
         // Tell Emscripten about our exported functions
-        println!("cargo:rustc-link-arg=-sEXPORTED_RUNTIME_METHODS=['ccall','cwrap']");
+        println!("cargo:rustc-link-arg=-sEXPORTED_RUNTIME_METHODS=['ccall','cwrap','UTF8ToString']");
         println!("cargo:rustc-link-arg=-sEXPORTED_FUNCTIONS=['_main','_solve_constraint','_free_string','_debug_message']");
         
         // Basic Emscripten configuration
@@ -16,5 +16,9 @@ fn main() {
         // Files to watch for changes
         println!("cargo:rerun-if-changed=build.rs");
         println!("cargo:rerun-if-changed=src/main.rs");
+
+        println!("cargo:rustc-link-arg=-sMODULARIZE=1");
+        println!("cargo:rustc-link-arg=-sEXPORT_ES6=1");
+        println!("cargo:rustc-link-arg=-sENVIRONMENT=web");
     }
 }
